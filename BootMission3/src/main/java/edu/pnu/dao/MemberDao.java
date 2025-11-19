eeceb68cdee4fa9b6657bf5bd42c463482888791
Kim Hyeon Ji <rlaguswl_01@naver.com>
2025-11-19 17:58:01 +0900

    미션4, 미션1,2 다시공부

--- BootMission3/src/main/java/edu/pnu/dao/MemberDao.java
+++ BootMission3/src/main/java/edu/pnu/dao/MemberDao.java
@@ -4,51 +4,143 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 import edu.pnu.domain.MemberDTO;
-import edu.pnu.controller.MemberController;
 
 public class MemberDao {
 	public Connection con;
 	public Statement stmt;
 	public PreparedStatement psmt;
 	public ResultSet rs;
+
 	public MemberDao() {
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/bootmission", "musthave", "tiger");
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/bootmission", "musthave", "tiger");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
-	public MemberDTO addMemberDTO(MemberDTO memberDTO) {
-		MemberDTO m = null;
-		PreparedStatement psmt = null;
-		ResultSet rs = null;
+
+	// 검색(Read - select All)
+	public List<MemberDTO> getAllMember() {
+		List<MemberDTO> list = new ArrayList<>();
+
+		String query = "select * from member";
 		try {
-			psmt = con.prepareStatement("insert into member(pass, name) values(?, ?)", Statement.RETURN_GENERATED_KEYS);
+			stmt = con.createStatement();
+			rs = stmt.executeQuery(query);
+
+			while (rs.next()) {
+				MemberDTO dto = new MemberDTO();
+				dto.setId(rs.getInt("id"));
+				dto.setPass(rs.getString("pass"));
+				dto.setName(rs.getString("name"));
+				dto.setRegidate(rs.getDate("regidate"));
+
+				list.add(dto);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return list;
+	}
+
+	// 검색(Read – select One)
+	public MemberDTO getMemberById(Integer id) {
+		MemberDTO dto = new MemberDTO();
+		String query = "select * from member where id=?";
+		try {
+			psmt = con.prepareStatement(query);
+			psmt.setInt(1, id);
+			rs = psmt.executeQuery();
+
+			while (rs.next()) {
+				dto.setId(rs.getInt("id"));
+				dto.setPass(rs.getString("pass"));
+				dto.setName(rs.getString("name"));
+				dto.setRegidate(rs.getDate("regidate"));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} 
+		return dto;
+	}
+
+	// 입력(Create - insert)
+	public MemberDTO postMember(MemberDTO memberDTO) {
+		MemberDTO dto = null;
+		String query = "insert into member(pass, name) values (?, ?)";
+		try {
+			psmt = con.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
 			psmt.setString(1, memberDTO.getPass());
 			psmt.setString(2, memberDTO.getName());
-			
-			if(psmt.executeUpdate() == 1) {
+
+			if (psmt.executeUpdate() == 1) {
 				rs = psmt.getGeneratedKeys();
-				if(rs.next()) {
+				if (rs.next()) {
 					int id = rs.getInt(1);
-					m = getMemberById(id);
+					dto = getMemberById(id);
 				}
 			}
-			System.out.println(psmt.toString().split(": ")[1]);
-		} catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
-		} finally {
-			try {
-				if(rs != null) rs.close();
-				if(psmt != null) psmt.close();
-			} catch(Exception e) {}
 		}
-		return null;
+		return dto;
+	}
+
+	// 수정(Update – 객체 교체)
+	public MemberDTO putMember(Integer id, MemberDTO memberDTO) {
+		MemberDTO dto = null;
+		String query = "update member set pass=?, name=? where id=?";
+		try {
+			psmt = con.prepareStatement(query);
+			psmt.setString(1, memberDTO.getPass());
+			psmt.setString(2, memberDTO.getName());
+			psmt.setInt(3, id);
+
+			if (psmt.executeUpdate() == 1) {
+				dto = getMemberById(id);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return dto;
+	}
+
+	// 수정(Update – 일부정보수정)
+	public MemberDTO patchMember(Integer id, MemberDTO memberDTO) {
+		MemberDTO dto = getMemberById(id);
+
+		String query = "update member set pass=ifnull(?, pass), name=ifnull(?, name) where id=?";
+		try {
+			psmt = con.prepareStatement(query);
+			psmt.setString(1, memberDTO.getPass());
+			psmt.setString(2, memberDTO.getName());
+			psmt.setInt(3, id);
+
+			if (psmt.executeUpdate() == 1) {
+				dto = getMemberById(id);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return dto;
+	}
+
+	public void deleteMember(Integer id) {
+		String query = "delete from member where id=?";
+		try {
+			psmt = con.prepareStatement(query);
+			psmt.setInt(1, id);
+			psmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
